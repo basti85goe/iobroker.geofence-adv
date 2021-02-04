578fee8ffbc93bb6ebcc7170e6d6197a70912e6e
Sebastian Schwill <privat@konzeptplus.net>
2021-02-04 16:01:01 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'geofence-adv adapter settings': {
+		'en': 'Adapter settings for geofence-adv',
+		'de': 'Adaptereinstellungen für geofence-adv',
+		'ru': 'Настройки адаптера для geofence-adv',
+		'pt': 'Configurações do adaptador para geofence-adv',
+		'nl': 'Adapterinstellingen voor geofence-adv',
+		'fr': "Paramètres d'adaptateur pour geofence-adv",
+		'it': "Impostazioni dell'adattatore per geofence-adv",
+		'es': 'Ajustes del adaptador para geofence-adv',
+		'pl': 'Ustawienia adaptera dla geofence-adv',
+		'zh-cn': 'geofence-adv的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
